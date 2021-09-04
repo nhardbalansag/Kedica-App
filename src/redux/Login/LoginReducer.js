5aa4de73d222bf209c268d6003e34d7e8117254a
nhardbalansag <nhardbalansag@gmail.com>
2021-09-05 06:12:02 +0800

    add folder structure and login screen

---
+++ src/redux/Login/LoginReducer.js
@@ -0,0 +1,14 @@
+const InitialStates ={
+    token: [],
+    data:[],
+}
+
+export default (state = InitialStates, action) =>{
+    switch(action.type){
+        default :
+            return{
+                ...state
+            }
+    }
+    return 
+}
