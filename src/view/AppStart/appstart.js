936fcd4b6369ce06680dc6442b850ef1f3493da9
nhardbalansag <nhardbalansag@gmail.com>
2021-09-07 02:37:37 +0800

    fix kedica login and add app start screen

--- src/view/AppStart/appstart.js
+++ src/view/AppStart/appstart.js
@@ -1,9 +1,56 @@
 import React from "react";
 
+import {
+    View,
+    Text
+} from "react-native"
+
+import {
+    styles,
+    colors
+} from "../../asset/css/BaseStyle";
+
 const AppStart = () =>{
     return(
-        <View>
-
+        <View 
+            style = {
+                [
+                    styles.flex1,
+                    styles.alignCenter,
+                    styles.justifyCenter
+                ]
+            }
+        >
+            <View>
+                <Text
+                    style ={
+                        [
+                            styles.font40,
+                            styles.textBold,
+                            styles.textWhite,
+                            styles.textUppercase
+                        ]
+                    }
+                >
+                    Kedica
+                </Text>
+            </View>
+            <View style={
+                [ 
+                    styles.justifyCenter, 
+                    styles.alignCenter,
+                    styles.w90
+                ] 
+            }>
+                <Text style={
+                    [
+                        styles.textCenter,
+                        styles.textWhite
+                    ]
+                }>
+                    © Copyright 2021 | All Rights Reserved | Powered by SEIKO-IT Solutions Inc.
+                </Text>
+            </View>
         </View>
     )
 }
