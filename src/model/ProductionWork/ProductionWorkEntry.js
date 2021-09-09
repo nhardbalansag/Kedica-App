13314d3fe05db2ee7fb3f504ca9142d2459b0e6e
Bernard Balansag <b.balansag@seiko-it.com.ph.local>
2021-09-09 14:13:12 +0800

    office - implement get all data in production work entry and displayto table

---
+++ src/model/ProductionWork/ProductionWorkEntry.js
@@ -0,0 +1,31 @@
+class ProductionWorkEntry {
+    constructor(
+        id,
+        Age,
+        PriorityNo,
+        ShipDate,
+        TravelSheetNo,
+        ItemCode,
+        ItemName,
+        StartDate,
+        StartProcess,
+        EndProcess,
+        SessionFirstName,
+        SessionLastName
+    ){
+        this.id
+        this.Age
+        this.PriorityNo
+        this.ShipDate
+        this.TravelSheetNo
+        this.ItemCode
+        this.ItemName
+        this.StartDate
+        this.StartProcess
+        this.EndProcess
+        this.SessionFirstName
+        this.SessionLastName
+    }
+}
+
+export default ProductionWorkEntry
